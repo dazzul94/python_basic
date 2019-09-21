6682e3140a401ed2d7841af1c362ffeb3f2a1568
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2019-09-21 15:25:30 +0900

    index.html 생성

--- Django_tutorial/mysite/polls/views.py
+++ Django_tutorial/mysite/polls/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 1. 스트링 리턴
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    # 2. 텍스트로 리턴
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+
+    # 3. index.html 리턴
+    template = loader.get_template('polls/index.html') # template 폴더에서 가져온다
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." %question_id)
