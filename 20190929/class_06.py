524b4381a03c228f67af675b5b3a0a484a51f448
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2019-09-29 15:33:59 +0900

    2019-09-25 15:33

--- 20190929/class_06.py
+++ 20190929/class_06.py
@@ -3,22 +3,11 @@
 # 클래스 선언
 class Circle:
     # 생성자
-    def __init__(self, name, korean, math, english, science):
-        self.name = name
-        self.korean = korean
-        self.math = math
-        self.english = english
-        self.science = science
+    def __init__(self, radius):
+        self.radius = radius
 
-    def get_sum(self):
-        return self.korean + self.math + self.english + self.science
+    def calcPerimeter(self):
+        return self.radius * 2 * 3.14
 
-    def get_average(self):
-        return self.get_sum() / 4
-    
-    def to_string(self):
-        return "{}\t{}\t{}".format(
-            self.name,
-            self.get_sum(),
-            self.get_average()
-        )
+    def calcArea(self):
+        return self.radius * self.radius * 3.14
